0695b5f2084c8e8c4a2691f20cf360e48a235e9f
Philip Corr <philip.corr@ucdconnect.ie>
2016-12-03 10:23:00 +0000

    created rename python script to ensure images are uniformly named

---
+++ Code/tensorflow/python/rename.py
@@ -0,0 +1,17 @@
+import os
+
+def rename(dir):
+  listOfFiles = os.listdir(dir)
+  print(listOfFiles)
+  for i in range(len(listOfFiles)):
+      os.rename(
+          os.path.join(dir, listOfFiles[i]),
+          os.path.join(dir, "image"+str(i).zfill(4)+".png")
+          )
+
+if __name__ == '__main__':
+  rename('/home/phil/Repos/cnn-gesture-recognition/db/images/')
+
+
+
+
